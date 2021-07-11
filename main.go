781700f3cc938f1d7f9b64311b7f70d26bff17ed
andrimarin <32032486+andrimarin@users.noreply.github.com>
2021-07-11 07:07:29 -0400

    Update main.go

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/johan-lejdung/go-microservice-api-guide/internal/app"
-	"github.com/johan-lejdung/go-microservice-api-guide/internal/db"
+	"github.com/andrimarin/go-microservice-api-guide/internal/app"
+	"github.com/andrimarin/go-microservice-api-guide/internal/db"
 )
 
 func main() {
